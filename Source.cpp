4692ad90ded529f08bdf4ea87591ceb48515f92d
Ryszard Mitka <Ryszard1191@gmail.com>
2020-04-21 11:46:18 +0200

    up

--- Source.cpp
+++ Source.cpp
@@ -1,12 +1,34 @@
 #include <iostream>
-#include "TestsGenerator.h"
+#include <string>
 using namespace std;
+void  turn_string(string& object, int step)
+{
+	if (step < 0)
+	{
+		for (int i = 0; i < (-1) * step; ++i)
+		{
+			char temp = object[0];
+			object.erase(object.begin());
+			object.push_back(temp);
+		}
+	}
+	else if (step > 0)
+	{
+		for (int i = 0; i < step; ++i)
+		{
+			char temp = object[object.size() - 1];
+			object.pop_back();
+			object.insert(object.begin(), temp);
+		}
+	}
+
+}
+
 
 
 int main()
 {
-	testgen test1(true);
-	char a = '2';
-	test1.screen("a", a);
-
+	string a = "test1";
+		turn_string(a, 1);
+	cout << a << endl;
 }
