523e35f5f7387cec6874f1a66ed3d7a1bf89550c
Ryszard Mitka <Ryszard1191@gmail.com>
2020-04-24 17:10:33 +0200

    search with sentinel

---
+++ searching - search_with_sentinel.h
@@ -0,0 +1,9 @@
+//T - array, n - size of array, k - key, p - start point
+int searchWithSentinel(int *T, int n, int k, int p=0)
+{
+	int i;
+
+	T[n] = k;
+	for (i = p; T[i] != k; i++);
+	if (i == n) return -1; else return i;
+}
